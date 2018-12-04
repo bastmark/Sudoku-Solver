6c7a97e063145dbeb9ac683a9afc7434fe40a4e6
Szymon Stypa <shabo.o@hotmail.com>
2018-12-04 12:45:01 +0100

    UI elements added with custom numer text field for out purposes

--- src/sudoku/Main.java
+++ src/sudoku/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("sudoku.fxml"));
+        primaryStage.setTitle("Sudoku Solver 0.1");
+        primaryStage.setScene(new Scene(root, 500, 350));
         primaryStage.show();
     }
 
